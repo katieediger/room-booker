9df42ced88bc185516ca1e77cee1dd93a24870b4
Katie Ediger <katieediger@gmail.com>
2018-03-14 10:07:37 -0500

    edited view for reservation index, and updated config/routes.rb to have reservations#index be the root for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'reservation/index'
 
+  root 'reservation#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
