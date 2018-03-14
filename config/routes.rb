7289596954504f9a387773d5ae6578293ba68391
Katie Ediger <katieediger@gmail.com>
2018-03-14 10:15:54 -0500

    added Bookings resource to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'reservation/index'
 
+  resources :bookings
+
   root 'reservation#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
